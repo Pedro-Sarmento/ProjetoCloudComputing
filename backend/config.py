ab3343b6d8e62899f49c9eb821d58a2a140760c3
Pedro Sarmento <77494920+Pedro-Sarmento@users.noreply.github.com>
2026-01-18 16:32:00 +0000

    final 2

--- backend/config.py
+++ backend/config.py
@@ -7,9 +7,8 @@ class Config:
     FLASK_SECRET_KEY = os.getenv("FLASK_SECRET_KEY")
     SESSION_COOKIE_HTTPONLY = True
     SESSION_COOKIE_SAMESITE = "Lax"   
-    SESSION_COOKIE_SECURE = False     
+    SESSION_COOKIE_SECURE = True     
 
     FRONTEND_ORIGINS = [
-        "http://127.0.0.1:5500",
-        "http://localhost:5500",
+        "https://Pedro-Sarmento.github.io"
     ]
